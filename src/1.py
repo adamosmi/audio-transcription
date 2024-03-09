608558c372c80c0b20edb523c8eefd749081697f
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-03-08 22:14:10 -0500

    Update transcription

--- src/1.py
+++ src/1.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from pyannote.audio import Pipeline
 import torch
 from pydub import AudioSegment
-import json
 import openai
+import json
 
 dotenv_path = os.path.join('config', '.env')
 load_dotenv(dotenv_path=dotenv_path)
@@ -16,9 +16,10 @@ OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 # file paths
 AUDIO_FP = os.path.join("data", "external", "transcription_test.wav")
-OUTPUT_FP = os.path.join("data", "processed", datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+OUTPUT_FP = os.path.join("data", "processed", datetime.now().strftime("%d_%m_%Y %H:%M:%S"))
 os.makedirs(OUTPUT_FP, exist_ok=True)
 
+print("Running diarization...")
 # start diarization pipeline
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-3.1",
@@ -36,11 +37,20 @@ for turn, _, speaker in diarization.itertracks(yield_label=True):
     diarization_results.append({"start": turn.start, "stop": turn.end, "speaker": speaker})
     print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
 
+print("Segmenting original audio file...")
 # load the original audio file for segmenting
 original_audio = AudioSegment.from_file(AUDIO_FP)
 
+diarization_results_full = {}
+
 # process each segment
 for i, segment_info in enumerate(diarization_results):
+    # segment output fp
+    SEGMENT_FP = os.path.join(OUTPUT_FP, f"segment_{i}_{segment_info['speaker']}.wav")
+
+    # store id
+    diarization_results_full[i] = {'fp': SEGMENT_FP, 'data': segment_info}
+
     # convert start and stop times to milliseconds
     start_ms = segment_info["start"] * 1000 // 1 # take floor
     stop_ms = (segment_info["stop"] * 1000 // 1) + 1 # take ceiling
@@ -49,23 +59,29 @@ for i, segment_info in enumerate(diarization_results):
     segment = original_audio[start_ms:stop_ms]
     
     # optionally, save the segment to a new file
-    segment.export(os.path.join(OUTPUT_FP, f"segment_{i}_{segment_info['speaker']}.wav"), format="wav")
+    segment.export(SEGMENT_FP, format="wav")
 
     # print confirmation
     print(f"Segment {i} for {segment_info['speaker']} saved.")
 
-# save json
-with open(os.path.join(OUTPUT_FP, 'diarization.json'), 'w') as f:
-    json.dump(obj=diarization_results, fp=f)
-
-# run transcriptions with openai whisper
-for file in os.listdir(OUTPUT_FP):
-    if file == 'diarization.json':
-        continue
-    print(file)
-
 # openai client
 client = openai.OpenAI(api_key=OPENAI_KEY)
 
+print("Transcribing...")
+for i, res in diarization_results_full.items():
+    # transcribe each file
+    audio_file = open(res['fp'], "rb")
+    transcription = client.audio.transcriptions.create(
+      model="whisper-1", 
+      file=audio_file
+    )
+
+    updated_res = res.copy()
+    updated_res['transcription'] = transcription.text
+    diarization_results_full[i] = updated_res
+
+# save json
+with open(os.path.join(OUTPUT_FP, 'analysis.json'), 'w') as f:
+    json.dump(obj=diarization_results_full, fp=f)
 
 

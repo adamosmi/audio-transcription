fb4c16e8d3ab588777b476f808db7f73bb180f91
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-03-10 18:28:44 -0400

    Change download format

--- src/get_audio.py
+++ src/get_audio.py
@@ -11,8 +11,8 @@ os.makedirs(OUTPUT_FP, exist_ok=True)
 
 yt = YouTube(VIDEO_URL)
 audio_streams = yt.streams.filter(only_audio=True)
-# download lowest quality audio stream to minimize file size
-stream = audio_streams.order_by('abr').first() # ascending sort on bit rate
+# download highest quality audio stream
+stream = audio_streams.order_by('abr').last() # ascending sort on bit rate, last is best
 
 stream_name = stream.title
 stream_subtype = stream.subtype
@@ -22,14 +22,16 @@ output_filename = '.'.join([stream_name, stream_subtype])
 
 stream.download(output_path=OUTPUT_FP, filename=output_filename)
 
-# convert into mp3 using ffmpeg
+# convert into wav using ffmpeg
+# ffmpeg -i input_file.wav -acodec pcm_s16le -ac 1 -ar 16000 -ab 256k output_file.wav
 command = [
     'ffmpeg',
     '-i', os.path.join(OUTPUT_FP, output_filename), # input
-    '-b:a', stream_bitrate_ffmpeg, # set bitrate to match input
-    '-ar', '16000', # 16kHz sample rate for pyannote
+    '-acodec', 'pcm_s16le' # wav codec
     '-ac', '1', # mono channel for pyannote
-    os.path.join(OUTPUT_FP, stream_name + '.mp3') 
+    '-ar', '16000', # 16kHz sample rate for pyannote
+    '-ab', '256k', # set bitrate
+    os.path.join(OUTPUT_FP, stream_name + '.wav') 
 ]
 
 subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
